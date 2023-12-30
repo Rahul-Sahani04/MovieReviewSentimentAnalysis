39deccd32c604943752bb8ae3d8dbaa85454391d
an <an@xyz.com>
2023-12-30 14:39:35 +0530

    Added How to Get IMDB Code img

--- App.py
+++ App.py
@@ -105,6 +105,7 @@ def perform_sentiment_analysis(movie_code):
 # Streamlit app
 st.title("IMDb Movie Reviews Sentiment Analysis")
 
+st.image("HowToGetIMDBCode.png", caption="How To Get IMDB Code", use_column_width=True)
 # Input field for IMDb code
 movie_code = st.text_input("Enter IMDb Code (e.g., tt12915716):")
 
